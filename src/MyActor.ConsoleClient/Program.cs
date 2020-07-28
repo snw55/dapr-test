a4f8a8b39fd41068dcc326dbd4f55b81b63bedc1
snw55 <jordy.noom@gmail.com>
2020-07-28 15:12:15 +0200

    Console client

---
+++ src/MyActor.ConsoleClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dapr.Actors;
+using Dapr.Actors.Client;
+
+using MyActor.Interfaces;
+
+namespace MyActor.ConsoleClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await InvokeActorMethodWithRemotingAsync();
+        }
+
+        static async Task InvokeActorMethodWithRemotingAsync()
+        {
+            var actorType = "MyActor";      // Registered Actor Type in Actor Service
+            var actorID = new ActorId("1");
+
+            // Create the local proxy by using the same interface that the service implements
+            // By using this proxy, you can call strongly typed methods on the interface using Remoting.
+            var proxy = ActorProxy.Create<IMyActor>(actorID, actorType);
+            var response = await proxy.SetDataAsync(new MyData()
+            {
+                PropertyA = "ValueA",
+                PropertyB = "ValueB",
+            });
+            Console.WriteLine(response);
+
+            var savedData = await proxy.GetDataAsync();
+            Console.WriteLine(savedData);
+        }
+    }
+}
